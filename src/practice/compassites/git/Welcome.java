683a83c5c06c0e2f04e722d22c9d70b2e2c284b4
shivammishra01 <mishra.shivam610@gmail.com>
2014-12-09 15:50:04 +0530

    First commit for a new branch

--- src/practice/compassites/git/Welcome.java
+++ src/practice/compassites/git/Welcome.java
@@ -4,6 +4,7 @@ class Welcome {
 	public static void main (String args[]){
 		System.out.println("This is first payment");
 		WelcomeSelenium dis= new WelcomeSelenium();
+		System.out.println("This is a new branch");
 		dis.display();
 	}
 
